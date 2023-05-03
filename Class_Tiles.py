65fc21f2c29ff4dbed14c205e37c23dc4ce48afd
GoldenPants <GoldenPants112@gmail.com>
2023-05-03 11:41:45 +0200

    Debut de la Classe Tiles

--- Class_Tiles.py
+++ Class_Tiles.py
@@ -1,3 +1,6 @@
 class Tiles:
-    def __init__(self) -> None:
-        pass
+    position=[0.0]
+    type=0
+    def __init__(self,_position,_type):
+        self.position=_position
+        self.type=_type
