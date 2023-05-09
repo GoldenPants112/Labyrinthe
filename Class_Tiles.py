55bcf50a8be973bb19790631d9111c8835437a7c
PrillBlizzard <103316116+PrillBlizzard@users.noreply.github.com>
2023-05-09 16:00:35 +0200

    Annulation chgmts

--- Class_Tiles.py
+++ Class_Tiles.py
@@ -1,11 +1,7 @@
-import pygame
+
 class Tiles:
     position=[0.0]
     type=0
     def __init__(self,_position,_type):
         self.position=_position
         self.type=_type
-    
-    def __repr__(self):
-        if (self.type == 1001) : # 1001 représente la case mur
-            pygame.draw.rectangle
