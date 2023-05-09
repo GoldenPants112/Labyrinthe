c8f7ee27e416e82198c822061f2e2e776af85f24
GoldenPants <GoldenPants112@gmail.com>
2023-05-09 17:17:21 +0200

    Now Rico Is small

--- Class_Game.py
+++ Class_Game.py
@@ -1,5 +1,6 @@
 import pygame
 
+
 #Creation de la premiere fentre
 pygame.init()
 
@@ -16,9 +17,11 @@ background_color = (255,255,255) #white
 #extracts Rico from file
 rico = pygame.image.load("Rico.png")
 
+# Scale the image to half its original size
+rico = pygame.transform.scale(rico, (rico.get_width() // 5, rico.get_height() // 5))
+
 # Get the dimensions of the image
 rico_width, rico_height = rico.get_size()
-
 running = True
 
 
@@ -33,9 +36,10 @@ while running:
     screen.fill(background_color)
     
     
+
     # Calculate the position of the image in the center of the screen
-    x = (pixelSize[0] - rico_width) // 2
-    y = (pixelSize[1] - rico_height) // 2
+    x = (pixelSize[0] - rico_width) // 2 #ordonne
+    y = (pixelSize[1] - rico_height) // 2 #abscisse
 
     # Draw the image on the screen
     screen.blit(rico, (x, y))
