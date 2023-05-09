04d332718c66fa123df9f3d1c4f289a64e12c1c8
GoldenPants <GoldenPants112@gmail.com>
2023-05-09 17:06:01 +0200

    Affiche Rico, a l'ecran

--- Class_Game.py
+++ Class_Game.py
@@ -9,21 +9,40 @@ pixelSize = (800,600)
 #Creation de l'ecran
 screen = pygame.display.set_mode(pixelSize)
 
-running = True
 
 background_color = (255,255,255) #white
 
-#while running:
-    
+
+#extracts Rico from file
+rico = pygame.image.load("Rico.png")
+
+# Get the dimensions of the image
+rico_width, rico_height = rico.get_size()
 
 running = True
+
+
 while running:
-    # Fill the screen with the background color
-    screen.fill(background_color)
-    
+    #if X -> close the window
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+
+    # Fill the screen with the background color
+    screen.fill(background_color)
     
+    
+    # Calculate the position of the image in the center of the screen
+    x = (pixelSize[0] - rico_width) // 2
+    y = (pixelSize[1] - rico_height) // 2
+
+    # Draw the image on the screen
+    screen.blit(rico, (x, y))
+
     # Update the screen
     pygame.display.update()
+
+
+
+    
