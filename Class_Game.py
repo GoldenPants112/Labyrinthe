1c42621021120f9b9e520d08efa76fe9a225384b
GoldenPants <GoldenPants112@gmail.com>
2023-05-09 16:05:06 +0200

    Creation de la Fenetre

---
+++ Class_Game.py
@@ -0,0 +1,18 @@
+import pygame
+
+#Creation de la premiere fentre
+pygame.init()
+
+#determination des dimentions de l'ecran
+pixelSize = (800,600)
+
+#Creation de l'ecran
+screen = pygame.display.set_mode(pixelSize)
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running == False
+
+pygame.quit() # Quit the game
