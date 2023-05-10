d2badf202e9b4e8c4ab0635f5e3fb9154ce81aee
PrillBlizzard <103316116+PrillBlizzard@users.noreply.github.com>
2023-05-10 17:19:37 +0200

    Continuation de la classe Room et Tiles
    Pour créer un objet d'un classe Room on aura juste besoinde l'ID de la Room, la salle et ainsi créer en fonction de ce dernier

--- Class_Room.py
+++ Class_Room.py
@@ -1,12 +1,20 @@
 import Class_Tiles
 class Room : 
-    map = [0,0]
     roomId = 0
+
     size = 0
-    def __init__(self,_maps,_Id,_size):
-        self.size = _size
-        self.map = _maps 
-        self.roomId = _Id
-   
+    map = [0,0]
+    
+    def __init__(self,_Id,):
+        self.roomId = _Id #ex :
+        #creation d'une map en fonction de roomID
+
+        if (self.roomID == 11):
+             self.size = 11
+             for i in self.size :
+                 for j in self.size:
+                     if (i == 0 or i == 11):
+                         bob # à finir
+    
     def getSize(self):
         return self.size
