43d3eb9ca9ed1cde64c1b0f1db4437e526f2193e
PrillBlizzard <103316116+PrillBlizzard@users.noreply.github.com>
2023-06-19 11:57:40 +0200

    Création de la fonction fin de jeu dans menu

--- menu.py
+++ menu.py
@@ -56,8 +56,8 @@ def main_menu(_screen,_taille_ecran,_textu_play_button,_textu_menu_bg) :
 def pause_menu(_screen,_taille_ecran,_textu_resume_button,_textu_pause_bg) :
     #on définie les dimensions des membres du menu
     pause_menu_size = []
-    pause_menu_size.append( _taille_ecran[0]/3)
-    pause_menu_size.append( _taille_ecran[0]/5)
+    pause_menu_size.append( _taille_ecran[0]*1/3)
+    pause_menu_size.append( _taille_ecran[1]*2/3)
 
     resume_button_size = []
     resume_button_size.append(60)
@@ -88,3 +88,18 @@ def pause_menu(_screen,_taille_ecran,_textu_resume_button,_textu_pause_bg) :
             if mouse_pos[0] > (resume_button_pos[0]) and mouse_pos[0] < (resume_button_pos[0]+resume_button_size[0]) :
                 if mouse_pos[1] > (resume_button_pos[1])  and mouse_pos[1] < (resume_button_pos[1]+resume_button_size[1]) :
                         checker = 0
+
+
+def engame(_screen,_taille_ecran,_textu_menu_bg):
+    _textu_menu_bg =  pygame.transform.scale(_textu_menu_bg,(_taille_ecran[0] , _taille_ecran[1] ) )
+    _screen.blit(_textu_menu_bg,(0,0))
+     
+    font = font = pygame.font.Font("Font/Raleway-Regular.ttf", 20)
+     
+    fst_sent = font.render("Félicitation vous avez fini le jeu !", True, (255, 255, 255))
+    snd_sent = font.render("Merci d'avoir jouer à cette petite démo sympathique", True, (255, 255, 255))
+    thd_sent = font.render("Ce jeu vous a été proposé par :", True, (255, 255, 255))
+    fourth_sent = font.render("BABA Hicham", True, (255, 255, 255))
+    fifth_sent = font.render("CHAUVET Gael", True, (255, 255, 255))
+
+    _screen.blit(fst_sent, (_taille_ecran[0]/2 - 40 , 10))
